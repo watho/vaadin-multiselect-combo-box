d650252a371c7d4036cddf37eedaaaa118a32764
watho <watho@users.noreply.github.com>
2019-02-15 08:32:25 +0100

    import vaadin-starter

---
+++ VaadinMultiselectComboBoxDemo/src/main/java/de/wathoserver/vaadin/MessageBean.java
@@ -0,0 +1,12 @@
+package de.wathoserver.vaadin;
+
+import java.time.LocalTime;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageBean {
+
+    public String getMessage() {
+        return "Button was clicked at " + LocalTime.now();
+    }
+}
